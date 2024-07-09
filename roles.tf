bb1643924ac7892d10afa7de4391474d2f938108
Shoki AIBA <shoki.aiba@pentio.com>
2024-07-09 16:07:46 +0900

    main.tf to roles.tf users.tf provider.tf

---
+++ roles.tf
@@ -0,0 +1,10 @@
+resource "onelogin_roles" "imported_role" {
+    admins = []
+    apps   = [
+        1472413,
+        1324452,
+        795681,
+    ]
+    name   = "terraform_test_role"
+    users  = []
+}
---
+++ users.tf
@@ -0,0 +1,17 @@
+resource "onelogin_users" "imported_user" {
+    activated_at           = "2024-07-09T06:11:53.751Z"
+    comment                = null
+    company                = "ペンティオ株式会社"
+    department             = null
+    email                  = "terraform@onelogin.jp"
+    firstname              = "test_updated"
+    invalid_login_attempts = 0
+    lastname               = "terraform"
+    phone                  = "08012345678"
+    role_ids               = []
+    state                  = 0
+    status                 = 1
+    title                  = null
+    updated_at             = "2024-07-09T06:11:53.752Z"
+    username               = "test.terraform"
+}
