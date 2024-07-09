8b7fc6d0e0ea4a8258e5d8dc30b13ff02d0155c5
Shoki AIBA <shoki.aiba@pentio.com>
2024-07-09 14:33:25 +0900

    Modify required version and Set apikey

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
-# main.tf
-
 terraform {
-  required_version = "= 1.1.3"
   required_providers {
     onelogin = {
       source  = "onelogin/onelogin"
-      version = "0.1.25"
+      version = "0.4.9"  
     }
   }
 }
 
 provider "onelogin" {
-  # Configuration options
+  apikey_auth = var.onelogin_apikey_auth
 }
